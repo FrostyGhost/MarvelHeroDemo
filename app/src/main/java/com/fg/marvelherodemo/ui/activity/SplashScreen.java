c37d3764696e382899209c4115510b0c21173840
FrostyGhost <frostyghost33@gmail.com>
2020-02-21 12:27:37 +0200

    Initial commit

---
+++ app/src/main/java/com/fg/marvelherodemo/ui/activity/SplashScreen.java
@@ -0,0 +1,32 @@
+package com.fg.marvelherodemo.ui.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import com.fg.marvelherodemo.R;
+
+public class SplashScreen extends AppCompatActivity {
+
+    Context ctx = this;
+    int splashInterval = 1000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_screen);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(ctx, MainActivity.class);
+                startActivity(i);
+                this.finish();
+            }
+            private void finish() {
+        }}, splashInterval);
+    }
+}
